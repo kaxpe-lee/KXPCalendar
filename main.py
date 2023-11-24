888fc4e19f1f0fa609f63fe17d1f924717000ac1
kaxpe-lee <fmartinez@work-art.net>
2023-11-24 12:57:06 +0100

    db

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify
+from flask import (Flask, redirect, session, render_template, request, send_from_directory, url_for)
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'miloja1502'
+app.config['DEBUG'] = True
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return "Hola Flask"
 
 
 if __name__ == '__main__':
